0f6fa1dc7014e91f7530ecfac5e943c09c603143
Solbeen0531 <netps0201@gmail.com>
2024-02-04 16:05:12 +0900

    Create 문자열_출력하기.java
    > print 함수 사용하여 문자열 출력
    > 스캐너 선언만 되어 있고, 닫혀 있지 않아 스캐너 닫음

---
+++ Programmers(프로그래머스)/기초 문제/문자열_출력하기.java
@@ -0,0 +1,26 @@
+// Java - Programmers - 코딩테스트 문제 - 기초 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+
+/* 기본 제공 코드
+ * public class 문자열_출력하기 {
+ * 	public static void main(String[] args) {
+ * 		Scanner sc = new Scanner(System.in);
+ * 		String a = sc.next();
+ * 	}
+ * } 
+*/
+
+package coding.practice.homework;
+
+import java.util.Scanner;
+
+public class 문자열_출력하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+
+		System.out.println(a);
+
+		sc.close();
+	}
+}
