ccb8ec86b550eabfaa2cca613af89a78434a222b
Solbeen0531 <netps0201@gmail.com>
2024-02-06 13:52:52 +0900

    Create a와_b_출력하기.java
    > scanner 닫기 추가
    > 정수형 a와 b의 이름 출력 추가

---
+++ Programmers(프로그래머스)/기초 문제/a와_b_출력하기.java
@@ -0,0 +1,33 @@
+package coding.practice.homework;
+
+//Java - Programmers - 코딩테스트 문제 - 기초 문제
+//https://school.programmers.co.kr/learn/courses/30/lessons/181951
+
+/* 기본 제공 코드
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
+*/
+
+import java.util.Scanner;
+
+public class a와_b_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        sc.close();        
+    }
+}
